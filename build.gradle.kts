f8cc35a6a090172334d1e094e8e3ffae49a72675
Hathaway Chen <hathaway1214@gmail.com>
2025-03-22 15:21:30 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,3 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
